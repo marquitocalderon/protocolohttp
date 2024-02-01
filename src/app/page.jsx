261d0ef05a1b553d27d4bf5c14b8c69e23d54ec4
Marco Calderon <marco.calderon1701@hotmail.com>
2024-02-01 00:20:18 -0500

    gaa

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,23 @@
+
+import axios from 'axios';
 import React from 'react'
 
-export default function page() {
+async function axiosDatetime() {
+  try {
+    const respuesta = await axios.get(`http://worldtimeapi.org/api/timezone/America/Lima`);
+    const datosServidor = respuesta.data;
+    return datosServidor;
+  } catch (error) {
+    console.error('Error al hacer la solicitud:', error);
+    throw error;
+  }
+}
+
+export default async function page() {
+
+  const data = await axiosDatetime();
+
   return (
-    <div>page</div>
+    <div>ESTO ES EN EL SERVIDOR {data.utc_datetime}</div>
   )
 }
